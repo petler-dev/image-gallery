3cd2b8506bfc7c493ee6525814dcc3d20f852dec
ypetler <yevhenii@webselenese.com>
2024-10-01 13:37:45 +0200

    Add image search and display using Unsplash API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './styles/App.scss';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('nature');
+
+  useEffect(() => {
+    axios
+      .get(`https://api.unsplash.com/search/photos?query=${query}&client_id=JUlsfiJRCcB5UiPDxbXeFkEtjP8rGnPUwq6Pspxt7ik`)
+      .then((response) => setImages(response.data.results));
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gallery-app">
+      <h1>Image Gallery</h1>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search images"
+      />
+      <div className="gallery">
+        {images.map((image) => (
+          <div key={image.id} className="gallery-item">
+            <img src={image.urls.small} alt={image.alt_description} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
